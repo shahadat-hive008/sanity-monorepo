1a0a33c6b295582576edc615dba2eadc991f5010
shahadat-hive008 <shahadat@lemonhive.com>
2026-04-22 18:14:01 +0600

    Create packages folder for query and type and also create typegen script

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'tozb2q8k',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,12 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
+  typegen: {
+    enabled: true,
+    path: '../../packages/sanity-queries/src/**/*.{ts,tsx,js,jsx}',
+    schema: './schemas/schema.json',
+    generates: '../../packages/sanity-types/src/sanity.types.ts',
+    overloadClientMethods: true,
+  },
 })
