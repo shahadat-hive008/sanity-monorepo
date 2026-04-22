b9a7b5cf91b3016677db40c415590ec40220244b
shahadat-hive008 <shahadat@lemonhive.com>
2026-04-22 18:15:30 +0600

    Create sanity queries folder where each query will be held

---
+++ packages/sanity-queries/src/index.ts
@@ -0,0 +1,8 @@
+/***
+*Here we export all the queries that are used in the app and we done that because next-sanity automatically *imports them using one public import.
+*Like when import BLOG_QUERY it will show like this
+*
+*import { BLOG_QUERY } from '@repo/sanity-queries';
+*
+***/
+export * from './blog';
